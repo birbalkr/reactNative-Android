668e81d1dbe5b5d211bf5244d15ff29672502d1f
birbalkr <birbalkr1435@gmail.com>
2026-05-31 21:31:30 +0530

    swipe navigation

---
+++ navigation/app/(swipe)/_layout.tsx
@@ -0,0 +1,19 @@
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import Home from ".";
+import About from "./about";
+import Setting from "./setting";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+const Tab = createMaterialTopTabNavigator();
+
+export default function TabLayout() {
+    return (
+        
+                <Tab.Navigator style={{ marginTop: 50 }}>
+                    <Tab.Screen name="Home" component={Home} />
+                    <Tab.Screen name="About" component={About} />
+                    <Tab.Screen name="Profile" component={Setting} />
+                </Tab.Navigator>
+           
+    );
+}
