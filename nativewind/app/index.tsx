acbd8f6e3cc21a679e8bcd10dc2387bb12fcebb3
birbalkr <birbalkr1435@gmail.com>
2026-06-01 10:44:00 +0530

    set up Tailwind CSS

---
+++ nativewind/app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import '../global.css';
+
+export default function Index() {
+  return (
+    <View className=" bg-black">
+      <Text className="color-white text-4xl">Home</Text>
+      <View className="bg-red-500 w-2/3 h-80 rounded-[25px] my-5 mx-auto">
+
+      </View>
+    </View>
+  );
+}
